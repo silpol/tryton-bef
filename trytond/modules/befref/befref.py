24376b4f2f1d7866b5e4fdb6b545f1ac0305d507
Vincent Mora <vincent.mora@oslandia.com>
2014-05-19 15:23:03 +0200

    first working version of qgis rendering

--- trytond/modules/befref/befref.py
+++ trytond/modules/befref/befref.py
@@ -37,11 +37,24 @@ from trytond.modules.geotools.tools import get_as_epsg4326, bbox_aspect
 from trytond.modules.map.map_render import MapRender
 from trytond.modules.qgis.qgis import QGis
 
+
+from urllib import urlopen
+from cStringIO import StringIO
+import shutil
+import tempfile
+import string
+import stat
+
+import time
+
 __all__ = ['Area']
 
 class Area(ModelSQL, ModelView):
     u'Protected area'
     __name__ = 'befref.area'
+
+    COLOR = (1, 0.1, 0.1, 1)
+    BGCOLOR = (1, 0.1, 0.1, 0.1)
 
     name = fields.Char(
             string=u'Site name',
@@ -55,4 +68,59 @@ class Area(ModelSQL, ModelView):
             select=True
         )
 
+    image = fields.Function(fields.Binary('Image'), 'get_image')
 
+    def get_image(self, ids):
+        if self.geom is None:
+            return buffer('')
+
+        tmpdir = tempfile.mkdtemp()
+        os.chmod(tmpdir, stat.S_IRUSR|stat.S_IWUSR|stat.S_IXUSR|stat.S_IXOTH|stat.S_IROTH)
+        dot_qgst = os.path.abspath('/home/vmo/test.qgst')
+        dot_qgs = os.path.join(os.path.abspath(tmpdir), 'proj.qgs')
+        with open(dot_qgst, 'r') as file_in, open(dot_qgs, 'w') as file_out:
+            tmp = string.Template(file_in.read())
+            file_out.write(tmp.substitute({'featureid': self.id}))
+
+        print '##################### get_image from project:', dot_qgs
+
+        cursor = Transaction().cursor
+        cursor.execute('SELECT ST_Extent(geom) FROM befref_area WHERE id = '+str(self.id)+';' )
+        [ext] = cursor.fetchone()
+        if ext:
+            ext = ext.replace('BOX(', '').replace(')', '').replace(' ',',')
+
+
+        #url = 'http://localhost/cgi-bin/qgis_mapserv.fcgi?SERVICE=WMS&VERSION=1.3.0&MAP='+dot_qgs+'&REQUEST=GetPrint&FORMAT=png&TEMPLATE=carte&LAYER=area&CRS=EPSG:2154&map0:EXTENT=973358,6458226,1036145,6510689&DPI=75'
+        url = 'http://localhost/cgi-bin/qgis_mapserv.fcgi?SERVICE=WMS&VERSION=1.3.0&MAP='+dot_qgs+'&REQUEST=GetPrint&FORMAT=png&TEMPLATE=carte&LAYER=area&CRS=EPSG:2154&map0:EXTENT='+ext+'&DPI=75'
+        start = time.time()
+        buf = buffer(urlopen(url).read())
+        end = time.time()
+        print end - start, 'sec to GetPrint ', url
+        
+        # TODO uncoment to cleanup, the directory and its contend are kept for debug
+        shutil.rmtree(tmpdir)
+
+        return buf
+
+        areas, envelope, _area = get_as_epsg4326([self.geom])
+        
+        if areas == []:
+            return buffer('')
+            
+        _envelope = bbox_aspect(envelope, 640, 480)    
+            
+        # Léger dézoom pour afficher correctement les aires qui touchent la bbox
+        envelope = [
+            _envelope[0] - 0.001,
+            _envelope[1] + 0.001,
+            _envelope[2] - 0.001,
+            _envelope[3] + 0.001,
+        ]                    
+
+
+
+        m = MapRender(640, 480, envelope, True)
+               
+        m.plot_geom(areas[0], None, None, color=self.COLOR, bgcolor=self.BGCOLOR)
+        return buffer(m.render())
